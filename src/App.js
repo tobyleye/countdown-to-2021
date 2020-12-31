6ad2c1362e42703f99b0c4d2e00f50a2d1292297
toby <krisella74@gmail.com>
2020-12-31 09:30:18 +0100

    fix overlapping text

--- src/App.js
+++ src/App.js
@@ -3,40 +3,34 @@ import "./styles.css";
 
 let year2021 = new Date(2021, 0, 1);
 
-let divmod = (numerator, denominator) => {
+const divmod = (numerator, denominator) => {
   let d = Math.floor(numerator / denominator);
   let remainder = numerator - denominator * d;
   return [d, remainder];
 };
-
 const millisecondsToDHMS = (ms) => {
   let [days, r1] = divmod(ms, 1000 * 60 * 60 * 24);
   let [hours, r2] = divmod(r1, 1000 * 60 * 60);
   let [minutes, r3] = divmod(r2, 1000 * 60);
   let [seconds] = divmod(r3, 1000);
-
   const result = { days, hours, minutes, seconds };
   // pad start each key value
   Object.keys(result).forEach((key) => {
     result[key] = result[key].toString().padStart(2, "0");
   });
-
   return result;
 };
 
 const Countdown = () => {
   const [, tick] = useReducer((c) => c + 1, 0);
-
   useEffect(() => {
     const timeout = setTimeout(() => {
       tick();
     }, 1000);
     return () => clearTimeout(timeout);
   });
-
   let remainingMilliSeconds = year2021 - new Date();
   const dhms = millisecondsToDHMS(remainingMilliSeconds);
-
   return (
     <div>
       <div className="countdown-to-next-year">
